3980d668531793f2599affeee32fb5c4696d8cd7
mackliu <macklun@gmail.com>
2026-01-13 15:48:47 +0800

    整理前後端版型及完成後台登入功能

--- admin.php
+++ admin.php
@@ -1,4 +1,15 @@
-<?php include_once "./api/db.php";?>
+<?php include_once "./api/db.php";
+if(isset($_POST['acc'])){
+  if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
+    $_SESSION['login']=1;
+  }else{
+    echo "<script>alert('帳號或密碼錯誤')</script>";
+  }
+}
+
+
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0055)?do=admin -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -15,13 +26,29 @@
   <div id="top" style=" background:#999 center; background-size:cover; " title="替代文字">
     <h1>ABC影城</h1>
   </div>
-  <div id="top2"> <a href="03P01.htm">首頁</a> <a href="03P02.htm">線上訂票</a> <a href="#">會員系統</a> <a href="03P03.htm">管理系統</a> </div>
+  <div id="top2"> 
+    <a href="index.php">首頁</a> 
+    <a href="index.php?do=order">線上訂票</a> 
+    <a href="#">會員系統</a> 
+    <a href="admin.php">管理系統</a> 
+  </div>
   <div id="text"> <span class="ct">最新活動</span>
     <marquee direction="right">
     ABC影城票價全面八折優惠1個月
     </marquee>
   </div>
   <div id="mm">
+
+  <?php 
+if(isset($_SESSION['login'])){
+?>
+<div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;"> 
+  <a href="#">網站標題管理</a>| 
+  <a href="#">動態文字管理</a>| 
+  <a href="?do=poster">預告片海報管理</a>| 
+  <a href="?do=movie">院線片管理</a>| 
+  <a href="?do=order">電影訂票管理</a> 
+</div>
   <?php 
   $do=$_GET['do']??'main';
   $file="./back/".$do.".php";
@@ -31,7 +58,33 @@
     include "./back/main.php";
   }
 
-  ?>
+}else{
+?>
+
+<form action="?" method="post">
+    <h2 class="ct">管理登入</h2>
+    <table style="width:50%;margin:auto">
+        <tr>
+            <td>帳號：</td>
+            <td>
+                <input type="text" name="acc" id="acc">
+            </td>
+        </tr>
+        <tr>
+            <td>密碼：</td>
+            <td>
+                <input type="password" name="pw" id="pw">
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="登入">
+    </div>
+</form>
+
+<?php
+}
+?>
     </div>
   <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
 </div>
