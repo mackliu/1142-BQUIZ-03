3e3b4f3ab621e2d985c612abcc116327ff5dfe92
mackliu <macklun@gmail.com>
2026-01-30 09:00:26 +0800

    根據訂單狀況計算各場次的剩餘座位

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -2,6 +2,7 @@
 include_once "db.php";
 
 $movieId=$_GET['movieId'];
+$movie=$Movie->find($movieId)['name'];  //取得電影名稱
 $date=strtotime($_GET['date']);
 
 $today=strtotime(date("Y-m-d"));
@@ -16,5 +17,9 @@ if($date==$today){
 
 
 for($i=$start;$i<=5;$i++){
-    echo "<option value='$i'>{$duration[$i]} 剩餘座位 20</option>";
+
+    //$sql="SELECT sum(`qt`) as 'qt' FROM `orders` WHERE movie='$movie' && date='{$_GET['date']}'  && session='{$duration[$i]}'";
+
+    $qt=20-$Order->sum('qt'," WHERE movie='$movie' && date='{$_GET['date']}'  && session='{$duration[$i]}'");
+    echo "<option value='$i'>{$duration[$i]} 剩餘座位 $qt</option>";
 }
